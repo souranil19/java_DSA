6e1e5c6d6361ab8c3334f5bc7e6afde57912cbdb
basak2005 <souranilbasak@gmail.com>
2026-05-18 19:14:16 +0530

    doubly linkedlist code base change

--- linked_list/DoublyLL.java
+++ linked_list/DoublyLL.java
@@ -1,148 +1,295 @@
 package linked_list;
+import java.util.Scanner;
 
-public class DoublyLL {
+class DNode
+{
+    int data;
+    DNode next;
+    DNode prev;
 
-    public class Node {
-        int data;
-        Node next;
-        Node prev;
-
-        // constructor
-        public Node(int data) {
-            this.data = data;
-            this.next = null;
-            this.prev = null;
-        }
+    DNode(int data)
+    {
+        this.data=data;
+        this.next=null;
+        this.prev=null;
     }
+}
 
-    public static Node head;
-    public static Node tail;
-    public static int size;
+class AddDNode
+{
+    public static DNode head;
+    public static DNode tail;
+    public static int size=0;
 
-    public void addFirst(int data) {
-        Node newNode = new Node(data);
-        size++;
-        if (head == null) {
-            head = tail = newNode;
+    public void addFirst(int data)
+    {
+        DNode newNode=new DNode(data);
+        size+=1;
+
+        if(head==null)
+        {
+            head=tail=newNode;
             return;
         }
 
-        newNode.next = head;
-        head.prev = newNode;
-        head = newNode;
-
-        // as the Node is increased
-
+        newNode.next=head;
+        head.prev=newNode;
+        head=newNode;
     }
 
-    public int removeFirst() {
-        int val = head.data;
-        if (head == null) {
-            System.out.println("Linked List is empty");
-            return Integer.MIN_VALUE;
-        }
-        if (size == 1) {
-            head = tail = null;
-            return val;
-        }
-
-        head = head.next;
-        head.prev = null;
-        size--;
-        return val;
-    }
-
-    public void addLast(int data) {
-        Node newNode = new Node(data);
-        size++;
-
-        if (head == null) {
-            head = newNode;
+    public void addMiddle(int idx, int data)
+    {
+        if(idx==0)
+        {
+            addFirst(data);
             return;
         }
 
-        tail.next = newNode;
-        newNode.prev = tail;
-        tail = newNode;
-
-    }
-
-    public int removeLast() {
-        size--;
-        int val = tail.data;
-        //corner case
-        //for no empty linked list
-        if (head == null) {
-            System.out.println("Linked List is empty");
-            return Integer.MIN_VALUE;
-        }
-        //for ll having only one value
-        if(head.next == null){
-            tail = head = null;
+        if(idx<0 || idx>size)
+        {
+            System.out.println("Invalid index");
+            return;
         }
 
-        tail = tail.prev;
-        tail.next = null;
-
-        return val;
-    }
-
-    public void print() {
-        // corner case
-        if (head == null) {
-            System.out.println("The Doubly Linked list is empty");
+        if(head==null)
+        {
+            throw new NullPointerException("No head so try to add first");
         }
 
-        Node temp = head;
-        while (temp != null) {
-            System.out.print(temp.data + "<->");
-            temp = temp.next;
-        }
-        System.out.println("Null");
-    }
-
-    public void reverse(){
-        Node curr = head;
-        Node prev = null;
-        Node next;
-
-        while(curr != null){
-            next = curr.next;
-            curr.next = prev;
-            curr.prev = next;
-
-            prev = curr;
-            curr = next;
+        int i=1;
+        DNode temp=head;
+        while(i<idx)
+        {
+            temp=temp.next;
+            i=i+1;
         }
 
-        head = prev;
+        DNode newNode=new DNode(data);
+        size+=1;
 
+        newNode.next=temp.next;
+        newNode.prev=temp;
 
+        if(temp.next!=null)
+        {
+            temp.next.prev=newNode;
+        }
+        else
+        {
+            tail=newNode;
+        }
+
+        temp.next=newNode;
     }
 
-    public static void main(String args[]) {
+    public void addLast(int data)
+    {
+        if(head==null)
+        {
+            addFirst(data);
+            return;
+        }
 
-        DoublyLL dll = new DoublyLL();
-        dll.addFirst(5);
-        dll.addFirst(4);
-        dll.addFirst(3);
-        dll.addFirst(2);
-        dll.addFirst(1);
+        DNode newNode=new DNode(data);
+        size+=1;
+        tail.next=newNode;
+        newNode.prev=tail;
+        tail=newNode;
+    }
+}
 
-        dll.print();
-        // System.out.println(dll.size);
-        // dll.removeFirst();
+class RemoveDNode
+{
+    public void firstRemove()
+    {
+        if (AddDNode.size == 0)
+        {
+            System.out.println("List is empty");
+            return;
+        }
 
-        // dll.addLast(6);
-        // dll.addLast(7);
-        // dll.print();
+        int val = AddDNode.head.data;
 
-        // dll.addLast(8);
-        // dll.addLast(9);
+        if (AddDNode.size == 1)
+        {
+            AddDNode.head = AddDNode.tail = null;
+            AddDNode.size -= 1;
+            System.out.println(val + " is deleted");
+            return;
+        }
 
-        dll.reverse();
-        dll.print();
-        
+        AddDNode.head = AddDNode.head.next;
+        AddDNode.head.prev = null;
+        AddDNode.size -= 1;
+        System.out.println(val + " is deleted");
     }
 
+    public void middleRemove(int idx)
+    {
+        if (AddDNode.size == 0)
+        {
+            System.out.println("List is empty");
+            return;
+        }
+
+        if (idx < 0 || idx >= AddDNode.size)
+        {
+            System.out.println("Invalid index");
+            return;
+        }
+
+        if (idx == 0)
+        {
+            firstRemove();
+            return;
+        }
+
+        if (idx == AddDNode.size - 1)
+        {
+            lastRemove();
+            return;
+        }
+
+        int i = 1;
+        DNode prev = AddDNode.head;
+        while (i < idx)
+        {
+            prev = prev.next;
+            i = i + 1;
+        }
+
+        DNode target = prev.next;
+        prev.next = target.next;
+        target.next.prev = prev;
+        AddDNode.size -= 1;
+        System.out.println(target.data + " is deleted");
+    }
+
+    public void lastRemove()
+    {
+        if (AddDNode.size == 0)
+        {
+            System.out.println("List is empty");
+            return;
+        }
+
+        int val = AddDNode.tail.data;
+
+        if (AddDNode.size == 1)
+        {
+            AddDNode.head = AddDNode.tail = null;
+            AddDNode.size -= 1;
+            System.out.println(val + " is deleted");
+            return;
+        }
+
+        AddDNode.tail = AddDNode.tail.prev;
+        AddDNode.tail.next = null;
+        AddDNode.size -= 1;
+        System.out.println(val + " is deleted");
+    }
+}
+
+class DUtility
+{
+    public void PrintNode(DNode head)
+    {
+        if(head==null)
+        {
+            System.out.print("List is empty");
+        }
+        else
+        {
+            DNode temp=head;
+            while(temp!=null)
+            {
+                System.out.print(temp.data + "<->");
+                temp = temp.next;
+            }
+            System.out.print("null");
+        }
+    }
+}
+
+public class DoublyLL
+{
+    public static void main(String args[])
+    {
+        Scanner scanner = new Scanner(System.in);
+        AddDNode add = new AddDNode();
+        RemoveDNode remove= new RemoveDNode();
+        DUtility util = new DUtility();
+        boolean running = true;
+
+        while(running)
+        {
+            System.out.println("\n1. Add First");
+            System.out.println("2. Add Middle");
+            System.out.println("3. Add Last");
+            System.out.println("4. Remove First");
+            System.out.println("5. Remove Middle");
+            System.out.println("6. Remove Last");
+            System.out.println("7. Print");
+            System.out.println("8. Exit");
+            System.out.print("Enter choice: ");
+
+            if(!scanner.hasNextInt())
+            {
+                System.out.println("Please enter a number.");
+                scanner.next();
+                continue;
+            }
+
+            int choice = scanner.nextInt();
+
+            switch(choice)
+            {
+                case 1:
+                    System.out.print("Enter data: ");
+                    add.addFirst(scanner.nextInt());
+                    break;
+                case 2:
+                    System.out.print("Enter index: ");
+                    int idx = scanner.nextInt();
+                    System.out.print("Enter data: ");
+                    int data = scanner.nextInt();
+                    try
+                    {
+                        add.addMiddle(idx, data);
+                    }
+                    catch(Exception ex)
+                    {
+                        System.out.println(ex.getMessage());
+                    }
+                    break;
+                case 3:
+                    System.out.print("Enter data: ");
+                    add.addLast(scanner.nextInt());
+                    break;
+                case 4:
+                    remove.firstRemove();
+                    break;
+                case 5:
+                    System.out.print("Enter index: ");
+                    int removeIdx = scanner.nextInt();
+                    remove.middleRemove(removeIdx);
+                    break;
+                case 6:
+                    remove.lastRemove();
+                    break;
+                case 7:
+                    util.PrintNode(AddDNode.head);
+                    System.out.println();
+                    break;
+                case 8:
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
+                    break;
+            }
+        }
+
+        scanner.close();
+    }
 }
